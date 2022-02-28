f6092240d3dc7eef2e1f3ddf5ccf96fb12debbe3
Kevin <kevin03.nguyen@gmail.com>
2022-02-28 11:47:00 -0500

    add images to blog

--- models/blog.js
+++ models/blog.js
@@ -4,6 +4,11 @@ const blogSchema = new mongoose.Schema({
   author: String,
   url: String,
   likes: { type: Number, default: 0 },
+  image: {
+    type: String,
+    default:
+      'https://cdn.iconscout.com/icon/free/png-256/list-message-2367725-1976875.png',
+  },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 });
 
